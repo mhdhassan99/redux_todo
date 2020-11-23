3d0e1bedc28dcd67a893efe78851b385c36ea6d0
mhdhassan99 <mhassan.work99@gmail.com>
2020-11-23 14:41:48 -0500

    stuck on eddit button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,33 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteTodo } from '../redux/action'
-
+import { useState } from 'react'
 const TodoItem = ( {todo} ) => {
     let dispatch = useDispatch()
+    const [editable, setEditable] = useState(false);
+    const [name, setName] = useState('');
+    debugger
     return (
         <div>
             <div className="row mx-2 align-items-center">
                 <div>#{todo.id.length > 1 ? todo.id[2] : todo.id}</div>
-                <div className="col" >{todo.name}</div>
-                <button className="btn btn-primary mx-2" >Edit</button>
+                <div className="col" >
+                    {editable ? <input type="text" className="form-control" 
+                        
+                        value={name}
+                        onChange={
+                            (e) => setName(e.target.value)
+                        }
+
+                    /> : <h4>{todo.name}</h4>}
+                </div>
+
+                <button 
+                    onClick={() => {
+                        setEditable(!editable);
+                    }}
+                    className="btn btn-primary mx-2" >
+                {editable ? "Update": "Edit"}</button>
                 <button className="btn btn-danger mx-2" onClick={() => dispatch(deleteTodo(todo.id))}> Delete</button>
             </div>
         </div>
